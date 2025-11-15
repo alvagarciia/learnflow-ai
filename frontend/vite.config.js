5d838376761b803aa8db81ff8ee19a6241bb585f
Alvaro Garcia <146588674+alvagarciia@users.noreply.github.com>
2025-11-15 11:51:01 -0600

    edit vite file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,16 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 5173,
     proxy: {
       '/api': {
         target: 'http://localhost:5000',
         changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
-  },
-  build: {
-    outDir: 'dist',
-    sourcemap: true,
   }
 })
